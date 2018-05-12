21b675b804d8398264acd0b83788ded0f899f131
alchemistmatt <matthew.monroe@pnnl.gov>
2018-05-11 17:24:47 -0700

    Update base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url']	= 'http://prismsupport.pnl.gov/smaqc/';
+$config['base_url']	= 'https://prismsupport.pnl.gov/smaqc/';
 
 /*
 |--------------------------------------------------------------------------
