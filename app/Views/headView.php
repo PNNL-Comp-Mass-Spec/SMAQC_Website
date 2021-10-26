9d4e69fc2637a7c40f106736661687ad9e2ce6b7
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2021-10-25 21:36:09 -0700

    Update the CSS and JavaScript paths

--- app/Views/headView.php
+++ app/Views/headView.php
@@ -17,26 +17,26 @@
 <head>
 <title>SMAQC</title>
 <meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="<?= base_url("assets/css/layout.css") ?>" />
-<link rel="stylesheet" type="text/css" href="<?= base_url("assets/css/present.css") ?>" />
-<link rel="stylesheet" type="text/css" href="<?= base_url("assets/css/smoothness/jquery-ui-1.8.17.custom.css") ?>" />
-<script type="text/javascript" src="<?= base_url("assets/js/jquery-1.7.1.min.js") ?>"></script>
-<script type="text/javascript" src="<?= base_url("assets/js/jquery-ui-1.8.17.custom.min.js") ?>"></script>
-<script type="text/javascript" src="<?= base_url("assets/js/calendar_pop.js") ?>"></script>
+<link rel="stylesheet" type="text/css" href="<?= base_url("css/layout.css") ?>" />
+<link rel="stylesheet" type="text/css" href="<?= base_url("css/present.css") ?>" />
+<link rel="stylesheet" type="text/css" href="<?= base_url("css/smoothness/jquery-ui-1.8.17.custom.css") ?>" />
+<script type="text/javascript" src="<?= base_url("js/jquery-1.7.1.min.js") ?>"></script>
+<script type="text/javascript" src="<?= base_url("js/jquery-ui-1.8.17.custom.min.js") ?>"></script>
+<script type="text/javascript" src="<?= base_url("js/calendar_pop.js") ?>"></script>
 
 <?php if( $includegraph ) :?>
     <!--[if IE]>
-    <script language="javascript" type="text/javascript" src="<?= base_url("assets/graph/excanvas.js") ?>"></script>
+    <script language="javascript" type="text/javascript" src="<?= base_url("graph/excanvas.js") ?>"></script>
     <![endif]-->
-    <script type="text/javascript" src="<?= base_url("assets/graph/jquery.jqplot.min.js") ?>"></script>
-    <script type="text/javascript" src="<?= base_url("assets/graph/plugins/jqplot.dateAxisRenderer.js") ?>"></script>
-    <script type="text/javascript" src="<?= base_url("assets/graph/plugins/jqplot.canvasTextRenderer.js") ?>"></script>
-    <script type="text/javascript" src="<?= base_url("assets/graph/plugins/jqplot.canvasAxisTickRenderer.js") ?>"></script>
-    <script type="text/javascript" src="<?= base_url("assets/graph/plugins/jqplot.canvasAxisLabelRenderer.min.js") ?>"></script>
-    <script type="text/javascript" src="<?= base_url("assets/graph/plugins/jqplot.highlighter.js") ?>"></script>
-    <script type="text/javascript" src="<?= base_url("assets/graph/plugins/jqplot.trendline.js") ?>"></script>
-    <script type="text/javascript" src="<?= base_url("assets/graph/plugins/jqplot.cursor.js") ?>"></script>
-    <link rel="stylesheet" href="<?= base_url("assets/graph/jquery.jqplot.min.css") ?>" type="text/css" media="all">
+    <script type="text/javascript" src="<?= base_url("graph/jquery.jqplot.min.js") ?>"></script>
+    <script type="text/javascript" src="<?= base_url("graph/plugins/jqplot.dateAxisRenderer.js") ?>"></script>
+    <script type="text/javascript" src="<?= base_url("graph/plugins/jqplot.canvasTextRenderer.js") ?>"></script>
+    <script type="text/javascript" src="<?= base_url("graph/plugins/jqplot.canvasAxisTickRenderer.js") ?>"></script>
+    <script type="text/javascript" src="<?= base_url("graph/plugins/jqplot.canvasAxisLabelRenderer.min.js") ?>"></script>
+    <script type="text/javascript" src="<?= base_url("graph/plugins/jqplot.highlighter.js") ?>"></script>
+    <script type="text/javascript" src="<?= base_url("graph/plugins/jqplot.trendline.js") ?>"></script>
+    <script type="text/javascript" src="<?= base_url("graph/plugins/jqplot.cursor.js") ?>"></script>
+    <link rel="stylesheet" href="<?= base_url("graph/jquery.jqplot.min.css") ?>" type="text/css" media="all">
     <script type="text/javascript">
         <?php
             // we need to set these dates, as well as other values in the Settings var
@@ -70,7 +70,7 @@
             plot.replot( {resetAxes: true } );
         });
     </script>
-    <script type="text/javascript" src="<?= base_url("assets/js/metricplot.js") ?>"></script>
+    <script type="text/javascript" src="<?= base_url("js/metricplot.js") ?>"></script>
 
 <?php endif; ?>
 
